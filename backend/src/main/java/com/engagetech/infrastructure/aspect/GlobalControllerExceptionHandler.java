65689b31685794553a6b2dcb6ec484f0c17f7e1c
Krzysztof Mosek <krzakov@gmail.com>
2017-07-14 10:38:04 +0200

    costmetics, unchecked casting warn resolved

--- backend/src/main/java/com/engagetech/infrastructure/aspect/GlobalControllerExceptionHandler.java
+++ backend/src/main/java/com/engagetech/infrastructure/aspect/GlobalControllerExceptionHandler.java
@@ -3,43 +3,44 @@ package com.engagetech.infrastructure.aspect;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Collections;
 import java.util.Map;
 
 @ControllerAdvice
+@ResponseBody
 @Slf4j
 public class GlobalControllerExceptionHandler {
 
 
     @ExceptionHandler(Exception.class) //one to rule them all !
-    public ResponseEntity<Map<String, String>> handleAllExceptions(HttpServletRequest request, Exception e){
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> handleAllExceptions(HttpServletRequest request, Exception e){
 
         log.info(String.format("General exception occured, URL: %s, message: %s",
                 request.getRequestURL(),
                 e.getMessage()));
 
-        return new ResponseEntity(Collections.singletonMap("error", "unexpected error, please contact engagetech support"),
-                HttpStatus.INTERNAL_SERVER_ERROR);
+        return Collections.singletonMap("error", "unexpected error, please contact engagetech support");
     }
 
     @ExceptionHandler(value = {DataIntegrityViolationException.class,
             HttpMessageNotReadableException.class,
             MethodArgumentNotValidException.class})
-    public ResponseEntity<Map<String, String>> handleDataException(HttpServletRequest request, Exception e){
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleDataException(HttpServletRequest request, Exception e){
 
         log.info(String.format("Data exception occured, URL: %s, cause: %s",
                 request.getRequestURL(),
                 e.getMessage()));
 
-        return new ResponseEntity(Collections.singletonMap("error", "malformed data error"),
-                HttpStatus.BAD_REQUEST);
+        return Collections.singletonMap("error", "malformed data error");
     }
-
 }
